6097360be70010174557f0014b652ae3607de6f5
YYL1999 <2291003927@qq.com>
2021-05-13 17:16:16 +0800

    feat(): webassembly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const ffmpeg = createFFmpeg({ log: true });
+    const transcode = async ({ target: { files } }) => {
+      const { name } = files[0];
+      await ffmpeg.load();
+      ffmpeg.FS('writeFile', name, await fetchFile(files[0]));
+      await ffmpeg.run('-i', name,  './main.mp4');
+      const data = ffmpeg.FS('readFile', './main.mp4');
+      console.log(new Blob([data.buffer]), 'cdc')
+      const video = document.getElementById('player');
+      video.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+      const download = document.getElementById('download');
+      download.href =  `${new Blob([data.buffer])}`;
+    }
+    document
+      .getElementById('uploader').addEventListener('change', transcode);
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +36,9 @@ function App() {
         >
           Learn React
         </a>
+        <video id="player" controls></video>
+        <input type="file" id="uploader"></input>
+        <a id="download">下载</a>
       </header>
     </div>
   );
